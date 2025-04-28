1e1ce320d4496d63072b9988fe363746112d5461
Francisco Lisboa <francisco.lisboa@poatek.com>
2025-04-28 13:17:40 -0300

    created base screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.searchField}>search</Text>
+        <Text style={styles.list}>list</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+  },
+  searchField: {
+    padding: 12,
+    backgroundColor: "green",
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 12,
   },
 });
